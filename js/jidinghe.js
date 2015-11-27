41b0651b7adfa6d80232e90ee92b1ba7e5f2a64f
JamesKid <406358964@qq.com>
2015-11-27 22:21:53 +0800

    update

--- js/jidinghe.js
+++ js/jidinghe.js
@@ -1,10 +1,26 @@
 $(document).ready(function(){
-	$(".imgHead").mousedown(function(){
+	var touchEvents = {
+        touchstart: "touchstart",
+        touchmove: "touchmove",
+        touchend: "touchend",
+
+        /**
+         * @desc:判断是否pc设备，若是pc，需要更改touch事件为鼠标事件，否则默认触摸事件
+         */
+        initTouchEvents: function () {
+            if (isPC()) {
+                this.touchstart = "mousedown";
+                this.touchmove = "mousemove";
+                this.touchend = "mouseup";
+            }
+        }
+    };
+	$(".imgHead").touchstart(function(){
 		alert('abc');
 		$("#up_01").css("display","none");
 		$("#up_01_down").css("display","block");
 	});
-	$(".imgHead").mouseup(function(){
+	$(".imgHead").touchend(function(){
 		alert('abc');
 		$("#up_01").css("display","block");
 		$("#up_01_down").css("display","none");
