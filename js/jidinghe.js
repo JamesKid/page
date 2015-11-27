837e7c8d2ef6add9bc5dfb8ef3c06078e5b95bf6
JamesKid <406358964@qq.com>
2015-11-27 22:45:47 +0800

    update

--- js/jidinghe.js
+++ js/jidinghe.js
@@ -3,7 +3,6 @@ $(document).ready(function(){
         touchstart: "touchstart",
         touchmove: "touchmove",
         touchend: "touchend",
-
         /**
          * @desc:判断是否pc设备，若是pc，需要更改touch事件为鼠标事件，否则默认触摸事件
          */
