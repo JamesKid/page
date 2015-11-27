0c9d2e0baa70e8f25bcde347e2b5e3723c921847
JamesKid <406358964@qq.com>
2015-11-27 23:14:53 +0800

    add fix

--- js/jidinghe.js
+++ js/jidinghe.js
@@ -20,9 +20,7 @@ $(document).ready(function(){
 	});
 	/* 长按事件 */
 	$(".imgHead").on("taphold",function(){
-		  alert('abc');
 		  $('img').css("point-event","none");
-		  return false;
 	});
 	$(".imgHead").mousedown(function(){
 		$("#up_01").css("display","none");
