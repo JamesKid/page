0d6e6ab8c4e58834c348da6f61a9f0f16110f0f1
JamesKid <406358964@qq.com>
2015-11-27 23:29:16 +0800

    update

--- js/jidinghe.js
+++ js/jidinghe.js
@@ -1,37 +1,38 @@
 $(document).ready(function(){
-	window.ontouchstart = function(e){e.preventDefault();};
-	var touchEvents = {
-        touchstart: "touchstart",
-        touchmove: "touchmove",
-        touchend: "touchend",
-        /**
-         * @desc:判断是否pc设备，若是pc，需要更改touch事件为鼠标事件，否则默认触摸事件
-         */
-        initTouchEvents: function () {
-            if (isPC()) {
-                this.touchstart = "mousedown";
-                this.touchmove = "mousemove";
-                this.touchend = "mouseup";
-            }
-        }
-    };
-	$(".imgHead").bind(touchEvents.touchstart,function(){
-		$("#up_01").css("display","none");
-		$("#up_01_down").css("display","block");
-	});
-	/* 长按事件 
-	$(".imgHead").on("taphold",function(){
-		  alert('acb');
-		  $('img').css("point-event","none");
-		  return false;
-	});
-	*/
-	$(".imgHead").mousedown(function(){
-		$("#up_01").css("display","none");
-		$("#up_01_down").css("display","block");
-	});
-	$(".imgHead").mouseup(function(){
-		$("#up_01").css("display","block");
-		$("#up_01_down").css("display","none");
-	});
+		var touchEvents = {
+			touchstart: "touchstart",
+			touchmove: "touchmove",
+			touchend: "touchend",
+			/**
+			 * @desc:判断是否pc设备，若是pc，需要更改touch事件为鼠标事件，否则默认触摸事件
+			 */
+			initTouchEvents: function () {
+				if (isPC()) {
+					this.touchstart = "mousedown";
+					this.touchmove = "mousemove";
+					this.touchend = "mouseup";
+				}
+			}
+		};
+		window.ontouchstart = function(e){
+			$(".imgHead").bind(touchEvents.touchstart,function(){
+				$("#up_01").css("display","none");
+				$("#up_01_down").css("display","block");
+			});
+		};
+		/* 长按事件 
+		$(".imgHead").on("taphold",function(){
+			  alert('acb');
+			  $('img').css("point-event","none");
+			  return false;
+		});
+		*/
+		$(".imgHead").mousedown(function(){
+			$("#up_01").css("display","none");
+			$("#up_01_down").css("display","block");
+		});
+		$(".imgHead").mouseup(function(){
+			$("#up_01").css("display","block");
+			$("#up_01_down").css("display","none");
+		});
 });
