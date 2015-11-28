bf5678e66268d27764421bbc97f4e931a20cbb1e
JamesKid <406358964@qq.com>
2015-11-28 22:07:42 +0800

    update

--- js/jidinghe.js
+++ js/jidinghe.js
@@ -16,17 +16,14 @@ $(document).ready(function(){
 		};
 		/* 上方按钮　*/
 		$(".imgHead").bind(touchEvents.touchstart,function(){
-			//alert('abc');
-			/* 解邦多点触摸 */
-			//$(".imgLeft").unbind(touchEvents.touchstart);
-		    //$(".imgLeft").attr(touchEvents.touchstart, "");  
 			$("#up_01").css("display","none");
 			$("#up_01_press").css("display","block");
 			$("#up_left").css("display","none");
+			/* 点击事件在下面添加 */
+
 			return false; /* 禁止长按 */
 		});
 		$(".imgHead").bind(touchEvents.touchend,function(){
-		    //$(".imgLeft").attr(touchEvents.touchend, "");  
 			$("#up_01").css("display","block");
 			$("#up_01_press").css("display","none");
 			$("#up_left").css("display","none");
@@ -42,11 +39,13 @@ $(document).ready(function(){
 		});
 		/* 左方按钮　*/
 		$(".imgLeft").bind(touchEvents.touchstart,function(){
-			$(".imgLeft").bind(touchEvents.touchstart,function(){ return false;});
 			$("#up_02").css("display","none");
 			$("#up_02_press").css("display","block");
 			$("#up_01").css("display","none");
 			$("#up_left").css("display","block");
+			$("#up_05").css("display","none");
+			$("#down_left").css("display","block");
+			/* 点击事件在下面添加 */
 			return false; /* 禁止长按 */
 		});
 		$(".imgLeft").bind(touchEvents.touchend,function(){
@@ -54,6 +53,8 @@ $(document).ready(function(){
 			$("#up_02_press").css("display","none");
 			$("#up_01").css("display","block");
 			$("#up_left").css("display","none");
+			$("#up_05").css("display","block");
+			$("#down_left").css("display","none");
 			return false; /* 禁止长按 */
 		});
 		$(".imgLeft").mousedown(function(){
@@ -61,18 +62,78 @@ $(document).ready(function(){
 			$("#up_02_press").css("display","block");
 			$("#up_01").css("display","none");
 			$("#up_left").css("display","block");
+			$("#up_05").css("display","none");
+			$("#down_left").css("display","block");
 		});
 		$(".imgLeft").mouseup(function(){
 			$("#up_02").css("display","block");
 			$("#up_02_press").css("display","none");
 			$("#up_01").css("display","block");
 			$("#up_left").css("display","none");
+			$("#up_05").css("display","block");
+			$("#down_left").css("display","none");
+		});
+		/* 中间按钮　*/
+		$(".imgCenter").bind(touchEvents.touchstart,function(){
+			$("#up_03").css("display","none");
+			$("#up_03_press").css("display","block");
+			/* 点击事件在下面添加 */
+			return false; /* 禁止长按 */
+		});
+		$(".imgCenter").bind(touchEvents.touchend,function(){
+			$("#up_03").css("display","block");
+			$("#up_03_press").css("display","none");
+			return false; /* 禁止长按 */
+		});
+		$(".imgCenter").mousedown(function(){
+			$("#up_03").css("display","none");
+			$("#up_03_press").css("display","block");
+		});
+		$(".imgCenter").mouseup(function(){
+			$("#up_03").css("display","block");
+			$("#up_03_press").css("display","none");
 		});
 		/* 右方按钮　*/
+		$(".imgRight").bind(touchEvents.touchstart,function(){
+			$("#up_04").css("display","none");
+			$("#up_04_press").css("display","block");
+			$("#up_01").css("display","none");
+			$("#up_right").css("display","block");
+			$("#up_05").css("display","none");
+			$("#down_right").css("display","block");
+			/* 点击事件在下面添加 */
+			return false; /* 禁止长按 */
+		});
+		$(".imgRight").bind(touchEvents.touchend,function(){
+			$("#up_04").css("display","block");
+			$("#up_04_press").css("display","none");
+			$("#up_01").css("display","block");
+			$("#up_right").css("display","none");
+			$("#up_05").css("display","block");
+			$("#down_right").css("display","none");
+			return false; /* 禁止长按 */
+		});
+		$(".imgRight").mousedown(function(){
+			$("#up_04").css("display","none");
+			$("#up_04_press").css("display","block");
+			$("#up_01").css("display","none");
+			$("#up_right").css("display","block");
+			$("#up_05").css("display","none");
+			$("#down_right").css("display","block");
+		});
+		$(".imgRight").mouseup(function(){
+			$("#up_04").css("display","block");
+			$("#up_04_press").css("display","none");
+			$("#up_01").css("display","block");
+			$("#up_right").css("display","none");
+			$("#up_05").css("display","block");
+			$("#down_right").css("display","none");
+		});
 		/* 下方按钮　*/
 		$(".imgFoot").bind(touchEvents.touchstart,function(){
 			$("#up_05").css("display","none");
 			$("#up_05_press").css("display","block");
+			/* 点击事件在下面添加 */
 			return false; /* 禁止长按 */
 		});
 		$(".imgFoot").bind(touchEvents.touchend,function(){
