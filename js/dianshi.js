9c28aa77caaa00ef2fb3611860d910dc4eebea15
JamesKid <406358964@qq.com>
2015-12-03 22:54:49 +0800

    update

--- js/dianshi.js
+++ js/dianshi.js
@@ -277,7 +277,6 @@ var SingletonTester = (function () {
 				}else if(upDown == 'down' && this.name=="center"){
 					$("#up_03").css("display","block");
 					$("#up_03_press").css("display","none");
-					this.name = 'noClick';
 				}
 			}else if(buttonName =="right" && this.name=='right'){
 				if(upDown == 'up' && this.name=="right"){
