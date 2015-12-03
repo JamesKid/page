cda87d6a19a51dc7726d07dd40da1512f0f2cc25
JamesKid <406358964@qq.com>
2015-12-03 22:01:31 +0800

    update

--- js/dianshi.js
+++ js/dianshi.js
@@ -1,9 +1,19 @@
-/* html5 电视遥控*/
+/* =========================================================================
+ * html5 电视遥控*/
 /* add by Jameskid 2015.11.28  email:406358964@qq.com */
-/* 使用说明:　 1. 请将app view关闭多点触控,关闭方法参考以下网址
- *					http://bbs.csdn.net/topics/390374889
- *			   2. 按钮事件请在'点击事件在下面添加'关键字下添加 
- *			   3. 本代码基于html5,请使用支持html5内核的浏览器
+/* 注意事项:　 
+ *			   1. 按钮事件请在行后封装好的方法下添加 
+ *			   2. 本代码基于html5,请使用支持html5内核的浏览器
+ *			   3. 禁止修改本代码，否则由此引发的代码错误，不提供维护,
+ *				  如必须修改，请联系本代码开发员
+ *				  
+ * 禁止修改本文件，点击事件请在 '*click.js' 文件添加
+ * 
+ * 禁止修改本文件，点击事件请在 '*click.js' 文件添加
+ *
+ * 禁止修改本文件，点击事件请在 '*click.js' 文件添加
+ *
+ * =========================================================================
  */
 $(document).ready(function(){
 		var touchEvents = {
@@ -45,19 +55,10 @@ $(document).ready(function(){
 				$('#nowStatus').text('开启');
 			}
 		});
+		/* 声音按钮 */
 		$("#sond_on").bind(touchEvents.touchstart,function(){
 			$("#sound_on").css("display","block");
 			$("#sound_off").css("display","none");
-			/*
-			if(sound%2==0){
-				$("#sound_on").css("display","none");
-				$("#sound_off").css("display","block");
-			}else if(sound%2==1){
-				$("#sound_on").css("display","block");
-				$("#sound_off").css("display","none");
-			}
-			sound++;
-			*/
 			/* 声音关闭事件请在此处添加代码 */
 		});
 		$("#sond_off").bind(touchEvents.touchstart,function(){
@@ -74,146 +75,238 @@ $(document).ready(function(){
 			$("#sound_off").css("display","none");
 		});
 
-
-		/* 上方按钮　*/
-		$(".imgHead").bind(touchEvents.touchstart,function(){
-			$("#up_01").css("display","none");
-			$("#up_01_press").css("display","block");
-			$("#up_left").css("display","none");
-			/* 点击事件在下面添加 */
-
-			return false; /* 禁止长按 */
-		});
-		$(".imgHead").bind(touchEvents.touchend,function(){
-			$("#up_01").css("display","block");
-			$("#up_01_press").css("display","none");
-			$("#up_left").css("display","none");
-			return false; /* 禁止长按 */
-		});
-		$(".imgHead").mousedown(function(){
-			$("#up_01").css("display","none");
-			$("#up_01_press").css("display","block");
-		});
-		$(".imgHead").mouseup(function(){
-			$("#up_01").css("display","block");
-			$("#up_01_press").css("display","none");
-		});
 		/* 左方按钮　*/
 		$(".imgLeft").bind(touchEvents.touchstart,function(){
-			$("#up_02").css("display","none");
-			$("#up_02_press").css("display","block");
-			$("#up_01").css("display","none");
-			$("#up_left").css("display","block");
-			$("#up_05").css("display","none");
-			$("#down_left").css("display","block");
-			/* 点击事件在下面添加 */
+			clickName = SingletonTester.getName(); 
+			if(clickName == 'noClick'){
+				SingletonTester.setName('left'); 
+				SingletonTester.setClickButton(SingletonTester.name,'left',0,'up'); 
+			}else {
+				return false;
+			}
 			return false; /* 禁止长按 */
 		});
 		$(".imgLeft").bind(touchEvents.touchend,function(){
-			$("#up_02").css("display","block");
-			$("#up_02_press").css("display","none");
-			$("#up_01").css("display","block");
-			$("#up_left").css("display","none");
-			$("#up_05").css("display","block");
-			$("#down_left").css("display","none");
-			return false; /* 禁止长按 */
+			clickName = SingletonTester.getName(); 
+			if(clickName == 'left'){
+				SingletonTester.setName('left'); 
+				SingletonTester.setClickButton(SingletonTester.name,'left',0,'down'); 
+			}else {
+				return false;
+			}
+			return false;  /* 禁止长按 */
 		});
 		$(".imgLeft").mousedown(function(){
-			$("#up_02").css("display","none");
-			$("#up_02_press").css("display","block");
-			$("#up_01").css("display","none");
-			$("#up_left").css("display","block");
-			$("#up_05").css("display","none");
-			$("#down_left").css("display","block");
+			SingletonTester.setName('left'); 
+			SingletonTester.setClickButton(SingletonTester.name,'left',0,'up'); 
 		});
 		$(".imgLeft").mouseup(function(){
-			$("#up_02").css("display","block");
-			$("#up_02_press").css("display","none");
-			$("#up_01").css("display","block");
-			$("#up_left").css("display","none");
-			$("#up_05").css("display","block");
-			$("#down_left").css("display","none");
+			SingletonTester.setName('left'); 
+			SingletonTester.setClickButton(SingletonTester.name,'left',0,'down'); 
 		});
-		/* 中间按钮　*/
-		var i = 0; /* 定义循环点击事件初始状态 */
-		$(".imgCenter").bind(touchEvents.touchstart,function(){
-			if(i%2==0){
-				$("#up_03").css("display","none");
-				$("#up_03_press").css("display","block");
-			}else if(i%2==1){
-				$("#up_03").css("display","block");
-				$("#up_03_press").css("display","none");
+		/* 上方按钮　*/
+		$(".imgHead").bind(touchEvents.touchstart,function(){
+			clickName = SingletonTester.getName(); 
+			if(clickName == 'noClick'){
+				SingletonTester.setName('head'); 
+				SingletonTester.setClickButton(SingletonTester.name,'head',0,'up'); 
+			}else {
+				return false;
 			}
-			i++;
-			/* 点击事件在下面添加 */
 			return false; /* 禁止长按 */
 		});
-		var i = 0; /* 定义循环点击事件初始状态 */
-		$(".imgCenter").mousedown(function(){
-			if(i%2==0){
-				$("#up_03").css("display","none");
-			$("#up_03_press").css("display","block");
-				$("#up_03_press").css("display","block");
-			}else if(i%2==1){
-				$("#up_03").css("display","block");
-				$("#up_03_press").css("display","none");
+		$(".imgHead").bind(touchEvents.touchend,function(){
+			clickName = SingletonTester.getName(); 
+			if(clickName == 'head'){
+				SingletonTester.setName('head'); 
+				SingletonTester.setClickButton(SingletonTester.name,'head',0,'down'); 
+			}else {
+				return false;
 			}
-			i++;
+			return false; /* 禁止长按 */
+		});
+		$(".imgHead").mousedown(function(){
+			SingletonTester.setName('head'); 
+			SingletonTester.setClickButton(SingletonTester.name,'head',0,'up'); 
+		});
+		$(".imgHead").mouseup(function(){
+			SingletonTester.setName('head'); 
+			SingletonTester.setClickButton(SingletonTester.name,'head',0,'down'); 
+		});
+		/*  中间按钮　*/
+		$(".imgCenter").bind(touchEvents.touchstart,function(){
+			clickName = SingletonTester.getName(); 
+			if(clickName == 'noClick'){
+				SingletonTester.setName('center'); 
+				SingletonTester.setClickButton(SingletonTester.name,'center',0,'up'); 
+			}else {
+				return false;
+			}
+			return false; /* 禁止长按 */
+		});
+		$(".imgCenter").bind(touchEvents.touchend,function(){
+			clickName = SingletonTester.getName(); 
+			if(clickName == 'center'){
+				SingletonTester.setName('center'); 
+				SingletonTester.setClickButton(SingletonTester.name,'center',0,'down'); 
+			}else {
+				return false;
+			}
+			return false; /* 禁止长按 */
+		});
+		$(".imgCenter").mousedown(function(){
+			SingletonTester.setName('center'); 
+			SingletonTester.setClickButton(SingletonTester.name,'center',0,'up'); 
+		});
+		$(".imgCenter").mouseup(function(){
+			SingletonTester.setName('center'); 
+			SingletonTester.setClickButton(SingletonTester.name,'center',0,'down'); 
 		});
 		/* 右方按钮　*/
 		$(".imgRight").bind(touchEvents.touchstart,function(){
-			$("#up_04").css("display","none");
-			$("#up_04_press").css("display","block");
-			$("#up_01").css("display","none");
-			$("#up_right").css("display","block");
-			$("#up_05").css("display","none");
-			$("#down_right").css("display","block");
-			/* 点击事件在下面添加 */
+			clickName = SingletonTester.getName(); 
+			if(clickName == 'noClick'){
+				SingletonTester.setName('right'); 
+				SingletonTester.setClickButton(SingletonTester.name,'right',0,'up'); 
+			}else {
+				return false;
+			}
 			return false; /* 禁止长按 */
 		});
 		$(".imgRight").bind(touchEvents.touchend,function(){
-			$("#up_04").css("display","block");
-			$("#up_04_press").css("display","none");
-			$("#up_01").css("display","block");
-			$("#up_right").css("display","none");
-			$("#up_05").css("display","block");
-			$("#down_right").css("display","none");
+			clickName = SingletonTester.getName(); 
+			if(clickName == 'right'){
+				SingletonTester.setName('right'); 
+				SingletonTester.setClickButton(SingletonTester.name,'right',0,'down'); 
+			}else {
+				return false;
+			}
 			return false; /* 禁止长按 */
 		});
 		$(".imgRight").mousedown(function(){
-			$("#up_04").css("display","none");
-			$("#up_04_press").css("display","block");
-			$("#up_01").css("display","none");
-			$("#up_right").css("display","block");
-			$("#up_05").css("display","none");
-			$("#down_right").css("display","block");
+			SingletonTester.setName('right'); 
+			SingletonTester.setClickButton(SingletonTester.name,'right',0,'up'); 
 		});
 		$(".imgRight").mouseup(function(){
-			$("#up_04").css("display","block");
-			$("#up_04_press").css("display","none");
-			$("#up_01").css("display","block");
-			$("#up_right").css("display","none");
-			$("#up_05").css("display","block");
-			$("#down_right").css("display","none");
+			SingletonTester.setName('right'); 
+			SingletonTester.setClickButton(SingletonTester.name,'right',0,'down'); 
 		});
 		/* 下方按钮　*/
 		$(".imgFoot").bind(touchEvents.touchstart,function(){
-			$("#up_05").css("display","none");
-			$("#up_05_press").css("display","block");
-			/* 点击事件在下面添加 */
+			clickName = SingletonTester.getName(); 
+			if(clickName == 'noClick'){
+				SingletonTester.setName('foot'); 
+				SingletonTester.setClickButton(SingletonTester.name,'foot',0,'up'); 
+			}else {
+				return false;
+			}
 			return false; /* 禁止长按 */
 		});
 		$(".imgFoot").bind(touchEvents.touchend,function(){
-			$("#up_05").css("display","block");
-			$("#up_05_press").css("display","none");
+			clickName = SingletonTester.getName(); 
+			if(clickName == 'foot'){
+				SingletonTester.setName('foot'); 
+				SingletonTester.setClickButton(SingletonTester.name,'foot',0,'down'); 
+			}else {
+				return false;
+			}
 			return false; /* 禁止长按 */
 		});
 		$(".imgFoot").mousedown(function(){
-			$("#up_05").css("display","none");
-			$("#up_05_press").css("display","block");
+			SingletonTester.setName('foot'); 
+			SingletonTester.setClickButton(SingletonTester.name,'foot',0,'up'); 
 		});
 		$(".imgFoot").mouseup(function(){
-			$("#up_05").css("display","block");
-			$("#up_05_press").css("display","none");
+			SingletonTester.setName('foot'); 
+			SingletonTester.setClickButton(SingletonTester.name,'foot',0,'down'); 
 		});
 });
+var SingletonTester = (function () { 
+	//参数：传递给单例的一个参数集合 
+	function Singleton(args) { 
+		this.name = 'noClick'; 
+	} 
+	//实例容器 
+	var instance; 
+	var _static = { 
+		name: 'noClick', 
+		//返回Singleton的实例 
+		getInstance: function (args) { 
+			if (instance === undefined) { 
+				instance = new Singleton(args); 
+			} 
+			return instance; 
+		} ,
+		setName: function (args) { 
+			this.name = args;
+		} ,
+		getName: function (args) { 
+			return this.name;
+		} ,
+		setClickButton: function (name,buttonName,number,upDown) { 
+			if(buttonName =='left' && this.name=='left'){
+				if(upDown == 'up' && this.name=='left'){
+					$("#up_02").css("display","none");
+					$("#up_02_press").css("display","block");
+					$("#up_01").css("display","none");
+					$("#up_left").css("display","block");
+					$("#up_05").css("display","none");
+					$("#down_left").css("display","block");
+				}else if(upDown == 'down' && this.name=="left"){
+					$("#up_02").css("display","block");
+					$("#up_02_press").css("display","none");
+					$("#up_01").css("display","block");
+					$("#up_left").css("display","none");
+					$("#up_05").css("display","block");
+					$("#down_left").css("display","none");
+					this.name = 'noClick';
+				}
+			}else if(buttonName =="head" && this.name=='head'){
+				if(upDown == 'up' && this.name=="head"){
+					$("#up_01").css("display","none");
+					$("#up_01_press").css("display","block");
+				}else if(upDown == 'down' && this.name=="head"){
+					$("#up_01").css("display","block");
+					$("#up_01_press").css("display","none");
+					this.name = 'noClick';
+				}
+			}else if(buttonName =="center" && this.name=='center'){
+				if(upDown == 'up' && this.name=="center"){
+					$("#up_03").css("display","none");
+					$("#up_03_press").css("display","block");
+				}else if(upDown == 'down' && this.name=="center"){
+					$("#up_03").css("display","block");
+					$("#up_03_press").css("display","none");
+					this.name = 'noClick';
+				}
+			}else if(buttonName =="right" && this.name=='right'){
+				if(upDown == 'up' && this.name=="right"){
+					$("#up_04").css("display","none");
+					$("#up_04_press").css("display","block");
+					$("#up_01").css("display","none");
+					$("#up_right").css("display","block");
+					$("#up_05").css("display","none");
+					$("#down_right").css("display","block");
+				}else if(upDown == 'down' && this.name=="right"){
+					$("#up_04").css("display","block");
+					$("#up_04_press").css("display","none");
+					$("#up_01").css("display","block");
+					$("#up_right").css("display","none");
+					$("#up_05").css("display","block");
+					$("#down_right").css("display","none");
+					this.name = 'noClick';
+				}
+			}else if(buttonName =="foot" && this.name=='foot'){
+				if(upDown == 'up' && this.name=="foot"){
+					$("#up_05").css("display","none");
+					$("#up_05_press").css("display","block");
+				}else if(upDown == 'down' && this.name=="foot"){
+					$("#up_05").css("display","block");
+					$("#up_05_press").css("display","none");
+					this.name = 'noClick';
+				}
+			}
+		} ,
+	}; 
+	return _static; 
+})(); 
