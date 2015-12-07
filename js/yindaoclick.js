e869560fc8861250623bf657ed02a45a8929fc91
JamesKid <406358964@qq.com>
2015-12-07 23:13:19 +0800

    update

---
+++ js/yindaoclick.js
@@ -0,0 +1,37 @@
+/* 引导页点击事件 */
+
+/*********** 使用说明 : 在function内添加代码 ****************/
+
+/* 开关点击返回事件组*/
+
+	/* 空调开关 */
+		/*　打开 */
+		function kongTiaoOnButton(){
+			alert('kon');
+		}
+		/*　关闭 */
+		function kongTiaoOffButton(){
+			alert('koff');
+		}
+
+	/* 电视开关 */
+		/*　打开 */
+		function dianShiOnButton(){
+			alert('don');
+		}
+		/*　关闭 */
+		function dianShiOffButton(){
+			alert('doff');
+		}
+	/* 机顶盒开关 */
+		/*　打开 */
+		function jiDingHeOnButton(){
+			alert('jon');
+		}
+		/*　关闭 */
+		function jiDingHeOffButton(){
+			alert('joff');
+		}
+
+
+
