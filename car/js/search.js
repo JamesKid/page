3e6192699c6beafdfc804741cc54228659e788c1
JamesKid <406358964@qq.com>
2016-02-19 00:29:00 +0800

    update

--- car/js/search.js
+++ car/js/search.js
@@ -1,9 +1,46 @@
 $(document).ready(function(){
-		/* 头部按钮 */
+	var touchEvents = {
+		touchstart: "touchstart",
+		touchmove: "touchmove",
+		touchend: "touchend",
+		initTouchEvents: function () {
+			if(isPC()) {
+				this.touchstart = "mousedown";
+				this.touchmove = "mousemove";
+				this.touchend = "mouseup";
+			}
+		}
+	};
+	/* 头部按钮 */
+	if(IsPC()){
 		$(".categoryWhite li").hover(function(){
 			$(this).css("background-color","#d82d24");
 			$(this).mouseleave(function(){
-				$(this).css("background-color","#e0e2e1");
+				$(this).css("background-color","transparent");
 			});
 		});
+	}else {
+		/*
+		$(".categoryWhite li").bind(touchEvents.touchstart,function(){
+			$(this).css("background-color","#d82d24");
+		});
+		$(".categoryWhite li").bind(touchEvents.touchend,function(){
+			$(this).css("background-color","#e0e2e1");
+		});
+		*/
+	}
 });
+function IsPC() {
+   var userAgentInfo = navigator.userAgent;
+   var Agents = ["Android", "iPhone",
+   "SymbianOS", "Windows Phone",
+   "iPad", "iPod"];
+   var flag = true;
+   for (var v = 0; v < Agents.length; v++) {
+	   if (userAgentInfo.indexOf(Agents[v]) > 0) {
+		   flag = false;
+		   break;
+	   }
+   }
+   return flag;
+}
